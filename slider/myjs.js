cec14a8a68697e745aafd1ff81fe5394693bfd3e
Peter <psesapu@gmail.com>
2019-12-07 14:42:13 +0300

    work on slider

--- slider/myjs.js
+++ slider/myjs.js
@@ -4,8 +4,13 @@ function Sim() {
 	this.sliderElements = this.sldrList.querySelectorAll(".sim-slider-element");
 	this.sliderElementFirst = this.sldrList.querySelector(".sim-slider-element");
 	this.sliderElementSecond = this.sldrList.querySelector(".sim-slider-element:nth-child(3)");
-	this.leftArrow = this.sldrList.querySelector(".sim-slider-arrow-left");
+	this.leftArrow = this.slider.querySelector(".sim-slider-arrow-left");
 	console.log(leftArrow);
+	this.rightArrow = this.slider.querySelector(".sim-slider-arrow-right");
+	console.log(rightArrow);
+
+	this.options = this.defaults;
+	Sim.inticialize(this);
 };
 
 Sim();
